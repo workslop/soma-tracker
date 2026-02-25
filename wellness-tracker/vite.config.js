47a00c2bcb3bc039cfcf32cb97cfaa02a27ad6a7
workslop <joesworkslop@gmail.com>
2026-02-24 22:05:36 -0500

    Update vite.config.js

--- wellness-tracker/vite.config.js
+++ wellness-tracker/vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({ jsxRuntime: 'classic' })],
+  build: {
+    sourcemap: false,
+  },
 })
